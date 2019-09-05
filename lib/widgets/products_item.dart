cb353bef30124a75450d676c4a146e062639cf9d
William Maphanga <williesdk@gmail.com>
2019-09-05 18:14:52 +0200

    bug fix on products item

--- lib/widgets/products_item.dart
+++ lib/widgets/products_item.dart
@@ -5,7 +5,8 @@ class ProductItem extends StatelessWidget {
   final String title;
   final String imageUrl;
 
-  ProductItem(this.id, this.title, this.imageUrl)
+  ProductItem(this.id, this.title, this.imageUrl);
+
   @override
   Widget build(BuildContext context) {
     return GridTile(
