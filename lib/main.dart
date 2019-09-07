973f7b963d73a51581e3051904a554d8aad35993
William Maphanga <williesdk@gmail.com>
2019-09-07 12:33:54 +0200

    bug fix on multiple providers

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: <SingleChildCloneableWidget>[
+      providers: [
         ChangeNotifierProvider.value(value: Products()),
         ChangeNotifierProvider.value(value: Cart()),
       ],
