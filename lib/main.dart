fa99f2acabc9c2efc85ee5dc3ab3e29b10fff860
William Maphanga <williesdk@gmail.com>
2019-09-07 12:11:08 +0200

    added multiple providers including cart

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,18 @@ import 'package:provider/provider.dart';
 import './screens/product_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
+import './providers/cart.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider.value(
-      value: Products(),
+    return MultiProvider(
+      providers: <SingleChildCloneableWidget>[
+        ChangeNotifierProvider.value(value: Products()),
+        ChangeNotifierProvider.value(value: Cart()),
+      ],
       child: MaterialApp(
         title: 'MyShop',
         theme: ThemeData(
