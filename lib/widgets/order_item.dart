13d4977024dfa7ad8d885642c7de24490ba2a91a
William Maphanga <williesdk@gmail.com>
2019-09-12 08:57:22 +0200

    added drop down icon function on order items list
    to view orders details

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,19 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import '../providers/orders.dart' as o;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final o.OrderItem orderItem;
 
   OrderItem(this.orderItem);
+
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -14,15 +22,48 @@ class OrderItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${orderItem.amount}'),
+            title: Text('\$${widget.orderItem.amount}'),
             subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(orderItem.dateTime),
+              DateFormat('dd/MM/yyyy hh:mm').format(widget.orderItem.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: (){},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height:
+                  min(widget.orderItem.products.length * 20.0 + 10.0, 180.0),
+              child: ListView(
+                children: widget.orderItem.products
+                    .map((prod) => Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              prod.title,
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text(
+                              '${prod.quantity} X \$${prod.price}',
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.grey,
+                              ),
+                            )
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
