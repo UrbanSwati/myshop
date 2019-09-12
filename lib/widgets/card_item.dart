261b649ca9eb1792f34e65cff06c8dbfdbbc3461
William Maphanga <williesdk@gmail.com>
2019-09-12 16:41:47 +0200

    added alert dialog on deleting an item

--- lib/widgets/card_item.dart
+++ lib/widgets/card_item.dart
@@ -16,6 +16,29 @@ class CardItem extends StatelessWidget {
     return Dismissible(
       key: ValueKey(id),
       direction: DismissDirection.endToStart,
+      confirmDismiss: (directiron) {
+        return showDialog(
+          context: context,
+          builder: (ctx) => AlertDialog(
+                title: Text('Are you sure?'),
+                content: Text('Do you want to remove item $title from cart'),
+                actions: <Widget>[
+                  FlatButton(
+                    child: Text('No'),
+                    onPressed: (){
+                      Navigator.of(context).pop(false);
+                    },
+                  ),
+                  FlatButton(
+                    child: Text('Yes'),
+                    onPressed: (){
+                      Navigator.of(context).pop(true);
+                    },
+                  )
+                ],
+              ),
+        );
+      },
       onDismissed: (_) {
         Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
