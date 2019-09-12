dbc001b630e3b287efdba3a35fa52379cec1d280
William Maphanga <williesdk@gmail.com>
2019-09-12 08:34:40 +0200

    refactored order item widget
    moved to widget folder

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../providers/orders.dart' as o;
 
 class OrderItem extends StatelessWidget {
-
   final o.OrderItem orderItem;
 
   OrderItem(this.orderItem);
 
   @override
   Widget build(BuildContext context) {
-    
     return Card(
       margin: EdgeInsets.all(10),
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${}'),
+            title: Text('\$${orderItem.amount}'),
+            subtitle: Text(
+              DateFormat('dd/MM/yyyy hh:mm').format(orderItem.dateTime),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.expand_more),
+              onPressed: (){},
+            ),
           )
         ],
       ),
-     );
+    );
   }
 }
