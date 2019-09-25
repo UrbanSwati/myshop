8fcf381c67c622696ae8917d7c2514ad4fb459a2
William Maphanga <williesdk@gmail.com>
2019-09-25 21:18:48 +0200

    added edit product on server
    not just locally

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -129,7 +129,22 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
+    final String baseUrl = environment['baseApiUrl'];
+    final url = '$baseUrl/products/$id,json';
+
+    await http.patch(
+      url,
+      body: json.encode(
+        {
+          'title': newProduct.title,
+          'description': newProduct.description,
+          'price': newProduct.price,
+          'imageUrl': newProduct.imageUrl
+        },
+      ),
+    );
+
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
       _items[prodIndex] = newProduct;
