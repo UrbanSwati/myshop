4f6b9925c88fa4fa3190af8c37240f9cda8857f1
William Maphanga <williesdk@gmail.com>
2019-09-26 17:36:01 +0200

    added orders to send to server

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import '../env.dart';
 import './cart.dart';
 
 class OrderItem {
@@ -21,16 +24,44 @@ class Orders with ChangeNotifier {
     return [..._orders];
   }
 
-  void addOrder(List<CartItem> cartProducts, double total) {
-    _orders.insert(
-      0,
-      OrderItem(
-        id: DateTime.now().toString(),
-        amount: total,
-        products: cartProducts,
-        dateTime: DateTime.now(),
-      ),
-    );
-    notifyListeners();
+  Future<void> addOrder(List<CartItem> cartProducts, double total) async {
+    final String baseUrl = environment['baseApiUrl'];
+    final url = '$baseUrl/orders.json';
+    final timeStamp = DateTime.now();
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'amount': total,
+            'products': [
+              cartProducts.map(
+                (cp) => {
+                  'id': cp.id,
+                  'title': cp.title,
+                  'price': cp.price,
+                  'quantity': cp.quantity,
+                },
+              ).toList()
+            ],
+            'dateTime': timeStamp.toIso8601String()
+          },
+        ),
+      );
+      if (response.statusCode <= 400) {
+        _orders.insert(
+          0,
+          OrderItem(
+            id: json.decode(response.body)['name'],
+            amount: total,
+            products: cartProducts,
+            dateTime: timeStamp,
+          ),
+        );
+        notifyListeners();
+      }
+    } catch (error) {
+      print(error);
+    }
   }
 }
