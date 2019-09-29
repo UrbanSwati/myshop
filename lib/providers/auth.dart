b9a1f6095c12a1b6e252017eb31fdc33149b1c7c
William Maphanga <williesdk@gmail.com>
2019-09-29 22:29:34 +0200

    modified auth provider to handle authentication

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -11,10 +11,24 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
+  bool get isAuth {
+    return token != null;
+  }
+
+  String get token {
+    if (_expiryDate != null &&
+        _expiryDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final key = environment['authKey'];
-    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$key';
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=$key';
 
     try {
       final response = await http.post(
@@ -25,11 +39,17 @@ class Auth with ChangeNotifier {
       );
       final responseData = json.decode(response.body);
       if (responseData['error'] != null) {
-        print(responseData['error']['message']);
         throw HttpException(responseData['error']['message']);
       }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _expiryDate = DateTime.now().add(
+        Duration(
+          seconds: int.parse(responseData['expiresIn']),
+        ),
+      );
+      notifyListeners();
     } catch (error) {
-      print(error);
       throw (error);
     }
   }
