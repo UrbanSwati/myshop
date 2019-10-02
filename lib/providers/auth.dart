7e92ef75764a0021e2e625a2c6429c8d7a9adee4
William Maphanga <williesdk@gmail.com>
2019-10-02 22:00:50 +0200

    added try login on start of app

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import '../models/http_exception.dart';
 
@@ -56,6 +57,10 @@ class Auth with ChangeNotifier {
       );
       _autoLogout();
       notifyListeners();
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode({'token': _token, 'userId': _userId, 'expiryDate': _expiryDate});
+      prefs.setString('userData', userData);
+
     } catch (error) {
       throw (error);
     }
@@ -69,7 +74,26 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, 'signInWithPassword');
   }
 
-  void logout(){
+  Future<bool> tryAutoLogin() async {
+      final prefs = await SharedPreferences.getInstance();
+      if(!prefs.containsKey('userData')){
+        return false;
+      }
+      final extractedUserData = json.decode(prefs.getString('userData')) as Map<String, Object>;
+      final expireDate = DateTime.parse(extractedUserData['expiryDate']);
+      if(expireDate.isAfter(DateTime.now())) {
+        return false;
+      }
+
+      _expiryDate = expireDate;
+      _token = extractedUserData['token'];
+      _userId = extractedUserData['userId'];
+      notifyListeners();
+      _autoLogout();
+      return true;
+  }
+
+  Future<void> logout() async{
     _expiryDate = null;
     _token = null;
     _userId = null;
@@ -77,6 +101,8 @@ class Auth with ChangeNotifier {
       _authTimer.cancel();
       _authTimer = null;
     }
+    final prefs = await SharedPreferences.getInstance();
+    prefs.clear() 
     notifyListeners();
   }
 
