92a7fd1bb27bd58bd9a8e1c02420a744b34a0263
William Maphanga <williesdk@gmail.com>
2019-10-02 20:51:38 +0200

    modified users products screen
    used future builder and consumer for products

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -9,13 +9,12 @@ class UserProductsScreen extends StatelessWidget {
   static const String routeName = '/user-products-screen';
 
   Future<void> _refreshProducts(BuildContext context) async {
-    await Provider.of<Products>(context, listen: false).fetchAndSetProducts(true);
+    await Provider.of<Products>(context, listen: false)
+        .fetchAndSetProducts(true);
   }
 
   @override
   Widget build(BuildContext context) {
-    final productsData = Provider.of<Products>(context);
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
@@ -29,19 +28,29 @@ class UserProductsScreen extends StatelessWidget {
         ],
       ),
       drawer: AppDrawer(),
-      body: RefreshIndicator(
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (ctx, i) {
-              final product = productsData.items[i];
-              return UserProductItem(
-                  product.id, product.title, product.imageUrl);
-            },
-          ),
-        ),
-        onRefresh: () => _refreshProducts(context),
+      body: FutureBuilder(
+        future: _refreshProducts(context),
+        builder: (ctx, snapshot) =>
+            snapshot.connectionState == ConnectionState.waiting
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : RefreshIndicator(
+                    child: Consumer<Products>(
+                      builder: (ctx, productsData, _) => Padding(
+                        padding: EdgeInsets.all(8),
+                        child: ListView.builder(
+                          itemCount: productsData.items.length,
+                          itemBuilder: (ctx, i) {
+                            final product = productsData.items[i];
+                            return UserProductItem(
+                                product.id, product.title, product.imageUrl);
+                          },
+                        ),
+                      ),
+                    ),
+                    onRefresh: () => _refreshProducts(context),
+                  ),
       ),
     );
   }
